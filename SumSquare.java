97e3f5b1b72dc02da2608789703c334a0a99af1b
jbroome2 <48440172+jbroome2@users.noreply.github.com>
2019-03-13 08:16:22 -0400

    Update SumSquare.java

--- SumSquare.java
+++ SumSquare.java
@@ -2,8 +2,15 @@ public class SumSquare
 {
 public static void main(String[] args)
 {
-java.util.Scanner input = new java.util.Scanner(System.in);
-System.out.println("Your program will generate an output of Sum of Squares of multiple of 3" + "\nfrom 3 to 33. ");
+ java.util.Scanner input = new java.util.Scanner(System.in);
+ System.out.println("Your program will generate an output of Sum of Squares of multiple of 3" + "\nfrom 3 to 33. ");
+ int i = 0;
+  i += 3;
+  System.out.print(Math.pow(i));
+  
+  
+  
+  
 }
 }
-  HIIIIIIIIIII
+  
